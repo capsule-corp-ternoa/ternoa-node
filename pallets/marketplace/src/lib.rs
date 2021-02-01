65f5b648d0b267616f4a34b2e1d15e008f8db6cd
Eliott Teissonniere <git.eliott@teissonniere.org>
2021-02-01 13:44:35 +0100

    listing function

--- pallets/marketplace/src/lib.rs
+++ pallets/marketplace/src/lib.rs
@@ -2,10 +2,10 @@
 
 use codec::Encode;
 use frame_support::{
-    decl_error, decl_event, decl_module, ensure,
+    decl_error, decl_event, decl_module, decl_storage, ensure,
     traits::{
         schedule::{DispatchTime, Named as ScheduleNamed},
-        LockIdentifier,
+        Currency, LockIdentifier,
     },
     Parameter,
 };
@@ -15,12 +15,11 @@ use ternoa_common::traits::{
     CapsuleCreationEnabled, CapsuleDefaultBuilder, CapsuleTransferEnabled,
 };
 
-/// Used for derivating scheduled tasks IDs
-const ESCROW_ID: LockIdentifier = *b"escrow  ";
-
 pub trait Trait: frame_system::Trait {
     /// Because this pallet emits events, it depends on the runtime's definition of an event.
     type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
+    /// Currency used to handle transactions and pay for the capsules.
+    type Currency: Currency<Self::AccountId>;
     /// Pallet managing capsules.
     type Capsules: CapsuleTransferEnabled<AccountId = Self::AccountId>
         + CapsuleCreationEnabled<
@@ -40,30 +39,32 @@ pub trait Trait: frame_system::Trait {
     type PalletsCall: Dispatchable<Origin = Self::Origin> + From<Call<Self>>;
 }
 
+type BalanceOf<T> =
+    <<T as Trait>::Currency as Currency<<T as frame_system::Trait>::AccountId>>::Balance;
 type CapsuleIDOf<T> = <<T as Trait>::Capsules as CapsuleTransferEnabled>::CapsuleID;
 
 decl_event!(
     pub enum Event<T>
     where
-        AccountId = <T as frame_system::Trait>::AccountId,
+        Balance = BalanceOf<T>,
         CapsuleID = CapsuleIDOf<T>,
-        BlockNumber = <T as frame_system::Trait>::BlockNumber,
     {
-        /// A transfer has been scheduled. \[capsule id, destination, block of transfer\]
-        TransferScheduled(CapsuleID, AccountId, BlockNumber),
-        /// A transfer has been canceled. \[capsule id\]
-        TransferCanceled(CapsuleID),
-        /// A transfer was executed and finalized. \[capsule id\]
-        TransferCompleted(CapsuleID),
+        /// A capsule has been listed for sale. \[capsule id, price\]
+        CapsuleListed(CapsuleID, Balance),
     }
 );
+
+decl_storage! {
+    trait Store for Module<T: Trait> as Marketplace {
+        /// Capsules listed on the marketplace
+        pub Capsules get(fn capules): map hasher(blake2_128_concat) CapsuleIDOf<T> => BalanceOf<T>;
+    }
+}
 
 decl_error! {
     pub enum Error for Module<T: Trait> {
         /// This function is reserved to the owner of a capsule.
         NotCapsuleOwner,
-        /// An unknown error happened which made the scheduling call fail.
-        SchedulingFailed,
     }
 }
 
@@ -71,62 +72,16 @@ decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
         fn deposit_event() = default;
 
-        /// Create a timed transfer. This will lock the associated capsule until it gets
-        /// transferred or canceled.
+        /// Deposit a capsule and list it on the marketplace
         #[weight = 0]
-        fn create(origin, capsule_id: CapsuleIDOf<T>, to: <T::Lookup as StaticLookup>::Source, at: T::BlockNumber) {
+        fn list(origin, capsule_id: CapsuleIDOf<T>, price: BalanceOf<T>) {
             let who = ensure_signed(origin)?;
-            Self::ensure_capsule_owner(who.clone(), capsule_id)?;
-            let to_unlookup = T::Lookup::lookup(to)?;
+            ensure!(T::Capsules::is_owner(who.clone(), capsule_id), Error::<T>::NotCapsuleOwner);
 
             T::Capsules::lock(capsule_id)?;
-            ensure!(T::Scheduler::schedule_named(
-                (ESCROW_ID, capsule_id).encode(),
-                DispatchTime::At(at),
-                None,
-                // priority was chosen arbitrarily, we made sure it is lower than runtime
-                // upgrades and democracy calls
-                100,
-                RawOrigin::Root.into(),
-                Call::complete_transfer(who, to_unlookup.clone(), capsule_id).into()
-            ).is_ok(), Error::<T>::SchedulingFailed);
+            Capsules::<T>::insert(capsule_id, price);
 
-            Self::deposit_event(RawEvent::TransferScheduled(capsule_id, to_unlookup, at));
+            Self::deposit_event(RawEvent::CapsuleListed(capsule_id, price));
         }
-
-        /// Cancel a transfer that was previously created and unlocks the capsule.
-        #[weight = 0]
-        fn cancel(origin, capsule_id: CapsuleIDOf<T>) {
-            let who = ensure_signed(origin)?;
-            Self::ensure_capsule_owner(who.clone(), capsule_id)?;
-
-            ensure!(T::Scheduler::cancel_named((ESCROW_ID, capsule_id).encode()).is_ok(), Error::<T>::SchedulingFailed);
-            T::Capsules::unlock(capsule_id)?;
-
-            Self::deposit_event(RawEvent::TransferCanceled(capsule_id));
-        }
-
-        /// System only. Execute a transfer, called by the scheduler.
-        #[weight = 0]
-        fn complete_transfer(origin, from: T::AccountId, to: T::AccountId, capsule_id: CapsuleIDOf<T>) {
-            ensure_root(origin)?;
-            T::Capsules::unlock(capsule_id)?;
-            T::Capsules::transfer_from(from, to, capsule_id)?;
-
-            Self::deposit_event(RawEvent::TransferCompleted(capsule_id));
-        }
-    }
-}
-
-impl<T: Trait> Module<T> {
-    fn ensure_capsule_owner(
-        maybe_owner: T::AccountId,
-        capsule_id: CapsuleIDOf<T>,
-    ) -> DispatchResult {
-        ensure!(
-            T::Capsules::is_owner(maybe_owner, capsule_id),
-            Error::<T>::NotCapsuleOwner
-        );
-        Ok(())
     }
 }
