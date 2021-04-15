a08380d057648b7adec9d2d14fd79038a162e5b5
Marko Petrlic <petrlic.marko@gmail.com>
2021-04-15 14:30:04 +0200

    updated the incorrectly entered weights for the burn extrinsic

--- pallets/nfts/src/default_weights.rs
+++ pallets/nfts/src/default_weights.rs
@@ -22,7 +22,7 @@ impl crate::WeightInfo for () {
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
     fn burn() -> Weight {
-        (51_000_000 as Weight)
+        (25_380_000 as Weight)
             .saturating_add(DbWeight::get().reads(1 as Weight))
             .saturating_add(DbWeight::get().writes(1 as Weight))
     }
