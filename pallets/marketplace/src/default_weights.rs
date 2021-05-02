12dd2800ea2df3423dcf5a02953524f853d92750
Marko Petrlic <petrlic.marko@gmail.com>
2021-05-02 12:05:59 +0200

    Updated marketplace buy benchmark

--- pallets/marketplace/src/default_weights.rs
+++ pallets/marketplace/src/default_weights.rs
@@ -2,9 +2,9 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 impl crate::WeightInfo for () {
     fn buy() -> Weight {
-        (38_210_000 as Weight)
+        (39_470_000 as Weight)
             .saturating_add(DbWeight::get().reads(2 as Weight))
-            .saturating_add(DbWeight::get().writes(1 as Weight))
+            .saturating_add(DbWeight::get().writes(2 as Weight))
     }
     fn list() -> Weight {
         (30_830_000 as Weight)
