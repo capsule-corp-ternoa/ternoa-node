3545a8065a37528f6f4a8a1e4835aac2471311bc
Marko Petrlic <petrlic.marko@gmail.com>
2021-05-03 13:49:54 +0200

    Updated nfts weights

--- pallets/nfts/src/default_weights.rs
+++ pallets/nfts/src/default_weights.rs
@@ -2,9 +2,9 @@ use frame_support::weights::{constants::RocksDbWeight as DbWeight, Weight};
 
 impl crate::WeightInfo for () {
     fn create() -> Weight {
-        (24_001_000 as Weight)
-            .saturating_add(DbWeight::get().reads(1 as Weight))
-            .saturating_add(DbWeight::get().writes(2 as Weight))
+        (34_540_000 as Weight)
+            .saturating_add(DbWeight::get().reads(3 as Weight))
+            .saturating_add(DbWeight::get().writes(4 as Weight))
     }
     fn mutate() -> Weight {
         (25_120_000 as Weight)
