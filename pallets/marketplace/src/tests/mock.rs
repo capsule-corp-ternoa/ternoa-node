bfdcdf55c6d849915ce7f08eef339b81aa2c3251
Marko Petrlic <petrlic.marko@gmail.com>
2021-07-17 09:56:27 +0200

    Fixed tests.

--- pallets/marketplace/src/tests/mock.rs
+++ pallets/marketplace/src/tests/mock.rs
@@ -7,7 +7,7 @@ use sp_runtime::{
     testing::Header,
     traits::{BlakeTwo256, IdentityLookup},
 };
-use ternoa_nfts::NFTDetails;
+use ternoa_primitives::nfts::NFTDetails;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
