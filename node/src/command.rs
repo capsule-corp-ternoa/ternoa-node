1a1a1bd02e3adbe6dc4607f7f0818fe3bf5e7709
Marko <petrlic.marko@gmail.com>
2021-08-20 11:05:03 +0200

    Added dev-remote chain.json

--- node/src/command.rs
+++ node/src/command.rs
@@ -37,6 +37,7 @@ impl SubstrateCli for Cli {
         Ok(match id {
             "chaos" => Box::new(chain_spec::chaos_config()),
             "dev" => Box::new(chain_spec::development_config()),
+            "dev-remote" => Box::new(chain_spec::dev_remote_config()),
             "local" => Box::new(chain_spec::local_testnet_config()),
             "staging" => Box::new(chain_spec::staging_testnet_config()),
             path => Box::new(chain_spec::ChainSpec::from_json_file(
