1c2aeaf9b8d5c7e2e55c33002a7bc88cf287baef
Marko Petrlic <petrlic.marko@gmail.com>
2022-02-07 10:27:40 +0100

    Added genesis test

---
+++ pallets/auctions/src/tests/genesis.rs
@@ -0,0 +1,42 @@
+use super::mock::*;
+use crate::types::{AuctionData, BidderList, DeadlineList};
+use crate::GenesisConfig;
+use frame_support::traits::GenesisBuild;
+
+#[test]
+fn genesis() {
+    let mut t = frame_system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap();
+
+    let bid_history_size = 100;
+    let auction = AuctionData {
+        creator: ALICE,
+        start_block: 10,
+        end_block: 20,
+        start_price: 10,
+        buy_it_price: Some(20),
+        bidders: BidderList::new(bid_history_size),
+        marketplace_id: ALICE_MARKET_ID,
+        is_extended: false,
+    };
+
+    let deadlines = DeadlineList(vec![(ALICE_NFT_ID, auction.end_block)]);
+    let auctions = vec![(ALICE_NFT_ID, auction)];
+
+    GenesisConfig::<Test> {
+        auctions: auctions.clone(),
+        bid_history_size,
+    }
+    .assimilate_storage(&mut t)
+    .unwrap();
+
+    let mut ext = sp_io::TestExternalities::new(t);
+    ext.execute_with(|| {
+        for auction in auctions {
+            assert_eq!(Auctions::auctions(auction.0), Some(auction.1));
+        }
+        assert_eq!(Auctions::bid_history_size(), bid_history_size);
+        assert_eq!(Auctions::deadlines(), deadlines);
+    });
+}
