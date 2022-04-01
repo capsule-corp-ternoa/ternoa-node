758202dab7fe32fc5063fee03f8de5e09f3fe1b4
Marko Petrlic <petrlic.marko@gmail.com>
2022-04-01 13:46:10 +0200

    Updates weights using a 4 core machine

--- scripts/benchmarks/alphanet/run_benchmakrs.sh
+++ scripts/benchmarks/alphanet/run_benchmakrs.sh
@@ -6,32 +6,33 @@ MEDIUM_COMMAND="./target/release/ternoa benchmark --chain alphanet-dev --steps=1
 SHORT_COMMAND="./target/release/ternoa benchmark --chain alphanet-dev --steps=5 --repeat=2 --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096 --output=./runtime/alphanet/src/weights/"
 
 echo "Running non-ternoa pallet benchmarks"
-# echo "PALLET BABE"
-# echo $(eval $COMMAND --pallet=pallet_babe)
 
-# echo "PALLET BAGS LIST"
-# echo $(eval $COMMAND --pallet=pallet_bags_list)
+echo "PALLET BABE"
+echo $(eval $COMMAND --pallet=pallet_babe)
 
-# echo "PALLET BALANCES"
-# echo $(eval $COMMAND --pallet=pallet_balances)
+echo "PALLET BAGS LIST"
+echo $(eval $COMMAND --pallet=pallet_bags_list)
 
-# echo "PALLET ELECTION PROVIDER MULTI PHASE"
-# echo $(eval $SHORT_COMMAND --pallet=pallet_election_provider_multi_phase)
+echo "PALLET BALANCES"
+echo $(eval $COMMAND --pallet=pallet_balances)
 
-# echo "PALLET GRANDPA"
-# echo $(eval $COMMAND --pallet=pallet_grandpa)
+echo "PALLET ELECTION PROVIDER MULTI PHASE"
+echo $(eval $SHORT_COMMAND --pallet=pallet_election_provider_multi_phase)
 
-# echo "PALLET IM ONLINE" 
-# echo $(eval $SHORT_COMMAND --pallet=pallet_im_online)
+echo "PALLET GRANDPA"
+echo $(eval $COMMAND --pallet=pallet_grandpa)
 
-# echo "PALLET MEMBERSHIP"
-# echo $(eval $COMMAND --pallet=pallet_membership)
+echo "PALLET IM ONLINE" 
+echo $(eval $SHORT_COMMAND --pallet=pallet_im_online)
 
-# echo "PALLET PREIMAGE"
-# echo $(eval $COMMAND --pallet=pallet_preimage)
+echo "PALLET MEMBERSHIP"
+echo $(eval $COMMAND --pallet=pallet_membership)
 
-# echo "PALLET SCHEDULER"
-# echo $(eval $COMMAND --pallet=pallet_scheduler)
+echo "PALLET PREIMAGE"
+echo $(eval $COMMAND --pallet=pallet_preimage)
+
+echo "PALLET SCHEDULER"
+echo $(eval $COMMAND --pallet=pallet_scheduler)
 
 # echo "TODO PALLET SESSION"
 # echo $(eval $COMMAND --pallet=pallet_session)
@@ -39,20 +40,20 @@ echo "Running non-ternoa pallet benchmarks"
 # echo "TODO PALLET STAKING"
 # echo $(eval $COMMAND --pallet=pallet_staking)
 
-# echo "PALLET TIMESTAMP"
-# echo $(eval $COMMAND --pallet=pallet_timestamp)
+echo "PALLET TIMESTAMP"
+echo $(eval $COMMAND --pallet=pallet_timestamp)
 
-# echo "PALLET TREASURY"
-# echo $(eval $COMMAND --pallet=pallet_treasury)
+echo "PALLET TREASURY"
+echo $(eval $COMMAND --pallet=pallet_treasury)
 
-# echo "PALLET UTILITY"
-# echo $(eval $COMMAND --pallet=pallet_utility)
+echo "PALLET UTILITY"
+echo $(eval $COMMAND --pallet=pallet_utility)
 
-# echo "PALLET COLLECTIVE"
-# echo $(eval $MEDIUM_COMMAND --pallet=pallet_collective)
+echo "PALLET COLLECTIVE"
+echo $(eval $MEDIUM_COMMAND --pallet=pallet_collective)
 
-# echo "PALLET COLLECTIVE"
-# echo $(eval $COMMAND --pallet=frame_system)
+echo "PALLET COLLECTIVE"
+echo $(eval $COMMAND --pallet=frame_system)
 
 echo "Running Ternoa pallet benchmarks"
 
