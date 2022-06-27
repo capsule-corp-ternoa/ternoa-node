ba708fc41be8fda39bf1765c9db01a824eb0befe
Marko Petrlić <petrlic.marko@gmail.com>
2022-06-27 14:28:06 +0200

    Release 1.1.0 (#221)

--- runtime/mainnet/src/pallets.rs
+++ runtime/mainnet/src/pallets.rs
@@ -434,7 +434,7 @@ impl ternoa_mandate::Config for Runtime {
 	type Event = Event;
 	type Call = Call;
 	type ExternalOrigin =
-		pallet_collective::EnsureProportionAtLeast<AccountId, TechnicalCollective, 2, 3>;
+		pallet_collective::EnsureProportionAtLeast<AccountId, TechnicalCollective, 1, 2>;
 }
 
 // Scheduler
@@ -594,4 +594,22 @@ impl pallet_identity::Config for Runtime {
 	type ForceOrigin = RootOrAtLeastHalfOfCommittee;
 	type RegistrarOrigin = RootOrAtLeastHalfOfCommittee;
 	type WeightInfo = pallet_identity::weights::SubstrateWeight<Runtime>;
+}
+
+parameter_types! {
+	pub const InitialMintFee: Balance = 10_000_000_000_000_000_000;
+	pub const NFTOffchainDataLimit: u32 = 150;
+	pub const CollectionOffchainDataLimit: u32 = 150;
+	pub const CollectionSizeLimit: u32 = 1_000_000;
+}
+
+impl ternoa_nft::Config for Runtime {
+	type Event = Event;
+	type WeightInfo = weights::ternoa_nft::WeightInfo<Runtime>;
+	type Currency = Balances;
+	type FeesCollector = Treasury;
+	type InitialMintFee = InitialMintFee;
+	type NFTOffchainDataLimit = NFTOffchainDataLimit;
+	type CollectionOffchainDataLimit = CollectionOffchainDataLimit;
+	type CollectionSizeLimit = CollectionSizeLimit;
 }
