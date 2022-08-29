fc0d53ea5f63fe91ce7c3db5e8200354f5fec17c
Marko Petrlić <petrlic.marko@gmail.com>
2022-08-29 17:00:32 +0200

    Marko/fix workflows (#268)
    * Fix for some workflows
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

--- .github/dockerimages/build-wasm.sh
+++ .github/dockerimages/build-wasm.sh
@@ -1,12 +1,21 @@
 # Build the binary in release mode
+# $HOME/.cargo/bin/cargo build --locked --release
+
+# Install srtool-cli
 $HOME/.cargo/bin/cargo install --git https://github.com/chevdor/srtool-cli
 
 # Move the wasm to a new folder called output
 mkdir -p output
-srtool build --package "$Runtime"-runtime &> output/output.txt
 
-cp ./target/release/ternoa ./output/
-cp ./runtime/"$Runtime"/target/srtool/release/wbuild/"$Runtime"-runtime/"$Runtime"_runtime.compact.compressed.wasm ./output/
-cp ./runtime/"$Runtime"/target/srtool/release/wbuild/"$Runtime"-runtime/"$Runtime"_runtime.compact.wasm ./output/
-cp ./runtime/"$Runtime"/target/srtool/release/wbuild/"$Runtime"-runtime/"$Runtime"_runtime.wasm ./output/
+# Move the ternoa-apllets file so that srtool doesn't complain
+mv ./ternoa-pallets/Cargo.toml ./ternoa-pallets/Cargo-copy.toml
+
+# Execute srtool
+sudo $HOME/.cargo/bin/srtool build --root --package "$runtime"-runtime &> output/output.txt
+
+cp ./runtime/"$runtime"/target/srtool/release/wbuild/"$runtime"-runtime/"$runtime"_runtime.compact.compressed.wasm ./output/
+cp ./runtime/"$runtime"/target/srtool/release/wbuild/"$runtime"-runtime/"$runtime"_runtime.compact.wasm ./output/
+cp ./runtime/"$runtime"/target/srtool/release/wbuild/"$runtime"-runtime/"$runtime"_runtime.wasm ./output/
+
+mv ./ternoa-pallets/Cargo-copy.toml ./ternoa-pallets/Cargo.toml
 
