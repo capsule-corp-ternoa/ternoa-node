a4ff5028bcf769b8bcdabfef2e1055c4c583570c
Marko Petrlic <petrlic.marko@gmail.com>
2022-09-06 21:45:02 +0200

    magic

--- scripts/run_benchmarks.sh
+++ scripts/run_benchmarks.sh
@@ -2,30 +2,49 @@
 
 # Default vaules
 DURATION="long"
-CHAIN="alphanet-dev"
+RUNTIME="alphanet"
 PALLET="*"
-OUTPUT="./weights/"
+OUTPUT_FOLDER="./weights/"
+STEPS=50
+REPEAT=20
+DEV=false
+PROD=false
 
 # Read flags
-while getopts d:c:p:o: flag
+while getopts d:r:p:o:dev:prod: flag
 do
     case "${flag}" in
         d) DURATION=${OPTARG};;
-        c) CHAIN=${OPTARG};;
+        r) RUNTIME=${OPTARG};;
         p) PALLET=${OPTARG};;
-        p) OUTPUT=${OPTARG};;
+        o) OUTPUT_FOLDER=${OPTARG};;
+        dev) DEV=true;;
+        prod) PROD=true;;
     esac
 done
+
+if $DEV; then
+    DURATION="short"
+fi
+
+if $PROD; then
+    DURATION="long"
+fi
 
 # echo "Building the Ternoa client..."
 # cargo build --profile production --locked --features=runtime-benchmarks
 
+CHAIN="$RUNTIME-dev"
+
 if [ "$DURATION" = "long" ]; then
-    COMMAND="./target/production/ternoa benchmark pallet --steps=50 --repeat=20 --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096"
+    STEPS=50
+    REPEAT=20
 elif [ "$DURATION" = "medium" ]; then
-    COMMAND="./target/production/ternoa benchmark pallet --steps=10 --repeat=5 --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096"
+    STEPS=10
+    REPEAT=5
 elif [ "$DURATION" = "short" ]; then
-    COMMAND="./target/production/ternoa benchmark pallet --steps=2 --repeat=1 --extrinsic=* --execution=wasm --wasm-execution=compiled --heap-pages=4096"
+    STEPS=2
+    REPEAT=1
 else 
     echo "Unknown duration. Supported value: long; medium; short"
     exit 0;
@@ -35,16 +54,59 @@ if [ "$OUTPUT" = "./weights/" ]; then
     mkdir -p weights
 fi
 
-if [ "$PALLET" = "*" ]; then
-    mkdir -p weights
-fi
-
-COMMAND="$COMMAND --pallet=$PALLET --chain $CHAIN --output=$OUTPUT"
-
 echo "Duration: $DURATION"
 echo "Chain: $CHAIN"
 echo "Pallet: $PALLET"
-echo "Output: $OUTPUT"
-echo "Command: $COMMAND"
+echo "Output folder: $OUTPUT_FOLDER"
+echo "Steps: $STEPS"
+echo "Repeat: $REPEAT"
 
-echo $(eval $COMMAND)
+
+# Manually exclude some pallets.
+EXCLUDED_PALLETS=(
+  # Helper pallets
+  "pallet_election_provider_support_benchmarking"
+  # Pallets without automatic benchmarking
+  "pallet_babe"
+  "pallet_grandpa"
+  "pallet_mmr"
+  "pallet_offences"
+)
+
+if [ "$PALLET" = "*" ]; then
+    PALLETS=($(./target/production/ternoa benchmark pallet --list --chain $CHAIN | tail -n+2 | cut -d',' -f1 | sort | uniq ))
+else
+    PALLETS=($PALLET)
+fi
+
+if [ "$OUTPUT_FOLDER" = "./weights/" ]; then
+    mkdir -p weights
+fi
+
+ERR_FILE="benchmarking_errors.txt"
+# Delete the error file before each run.
+rm -f $ERR_FILE
+
+# Benchmark each pallet.
+for PALLET in "${PALLETS[@]}"; do
+    SKIP=false
+    for EXCLUDED_PALLET in "${EXCLUDED_PALLETS[@]}"; do
+        if [ "$EXCLUDED_PALLET" == "$PALLET" ]; then
+            SKIP=true
+            break
+        fi
+    done
+
+    if $SKIP; then
+        echo "[ ] Skipping pallet $PALLET";
+        continue
+    fi
+
+    echo "[+] Benchmarking $PALLET";
+
+    OUTPUT=$(./target/production/ternoa benchmark pallet --chain=$CHAIN --steps=$STEPS --repeat=$REPEAT --pallet="$PALLET" --extrinsic="*" --execution=wasm --wasm-execution=compiled --heap-pages=4096 --output $OUTPUT_FOLDER 2>&1 )
+    if [ $? -ne 0 ]; then
+        echo "$OUTPUT" >> "$ERR_FILE"
+        echo "[-] Failed to benchmark $PALLET. Error written to $ERR_FILE; continuing..."
+    fi
+done
